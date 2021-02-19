2924513b23cb101fd1e25e3e0d66071f52b974f3
wlhtck <will@wlhtck.io>
2021-02-18 23:41:15 -0500

    Migrate the site to nextjs

---
+++ pages/_app.jsx
@@ -0,0 +1,18 @@
+// import App from 'next/app'
+import React from 'react'
+import '../public/css/default.css'
+import Head from 'next/head'
+
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <title>Will Hitchcock</title>
+    </Head>
+    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+    <Component {...pageProps} />
+  </>
+)
+
+export default MyApp
+
